5117c29033d0cdf0711e9791476846c18dba0e64
Om Lokhande <91490800+rubyproducti9n@users.noreply.github.com>
2026-04-26 19:13:14 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  publicDir: 'assets/icons',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
